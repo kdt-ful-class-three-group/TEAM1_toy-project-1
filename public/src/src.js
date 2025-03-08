ae79bd287481e9ff1594d1bc0f38a0b2d29de37e
윤종환 <yunjonghwan99@gmail.com>
2025-03-08 17:03:43 +0900

    Chore: 격자무늬 생성 -div 요소를 넣어줘서 격자 무늬를 생성한다.

--- public/src/src.js
+++ public/src/src.js
@@ -7,4 +7,10 @@ startBtn.addEventListener('click', () => {
   
   descDisplay.style.display = 'none';
   gameDisplay.classList.replace('d-none', 'd-grid');
+
+  for(let i=0; i<374; i++) {
+    const div = document.createElement('div');
+    div.classList.add('w-1rem', 'h-1rem', 'border-1px-dark');
+    gameDisplay.append(div);
+  }
 });
