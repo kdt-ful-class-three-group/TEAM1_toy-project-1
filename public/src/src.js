312106e1c81f459f5918370240475f6142f527e1
윤종환 <yunjonghwan99@gmail.com>
2025-03-08 17:05:57 +0900

    Chore: 격자 생성 코드 함수화 -격자로 생성되는 코드를 makeGrid이름의 함수로 만듦

--- public/src/src.js
+++ public/src/src.js
@@ -1,16 +1,18 @@
 const startBtn = document.querySelector('button');
+const articles = document.querySelectorAll('article');
+const descDisplay = articles[1];
+const gameDisplay = articles[2];
 
-startBtn.addEventListener('click', () => {
-  const articles = document.querySelectorAll('article');
-  const descDisplay = articles[1];
-  const gameDisplay = articles[2];
-  
-  descDisplay.style.display = 'none';
-  gameDisplay.classList.replace('d-none', 'd-grid');
-
+function makeGrid() {
   for(let i=0; i<374; i++) {
     const div = document.createElement('div');
     div.classList.add('w-1rem', 'h-1rem', 'border-1px-dark');
     gameDisplay.append(div);
   }
+}
+
+startBtn.addEventListener('click', () => {
+  descDisplay.style.display = 'none';
+  gameDisplay.classList.replace('d-none', 'd-grid');
+  makeGrid();
 });
