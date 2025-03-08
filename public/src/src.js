e3b557a2284c60d791bc5f6de28567134608ed04
윤종환 <yunjonghwan99@gmail.com>
2025-03-08 15:24:47 +0900

    Chore: 화면 교체 -시작 버튼 눌렀을 때 설명 화면에서 게임 화면으로 변경

--- public/src/src.js
+++ public/src/src.js
@@ -2,4 +2,10 @@ const startBtn = document.querySelector('button');
 
 startBtn.addEventListener('click', () => {
   const articles = document.querySelectorAll('article');
+  const descDisplay = articles[1];
+  const gameDisplay = articles[2];
+  
+  descDisplay.style.display = 'none';
+  gameDisplay.classList.remove('d-none');
+  gameDisplay.classList.add('d-block');
 });
