60cebe86b761695dbdc0a567c815ab5164ed507b
kimyohun <yohunkim0@gmail.com>
2025-03-12 15:02:11 +0900

    buttons 객체

---
+++ public/src/modules/buttons.js
@@ -0,0 +1,13 @@
+const buttons = {
+    startBtn : document.querySelectorAll('button')[0],
+    resetBtn : document.querySelectorAll('button')[1]
+  }
+  
+  // //* button태그들을 모두 받아와서 buttons에 담아줌
+  // const buttons = document.querySelectorAll('button');
+  // //* "시작 하기"버튼요소를 받아와서 startBtn에 담아줌
+  // const startBtn = buttons[0];
+  // //* "다시하기" 버튼 요소를 받아와서 resetBtn에 담아줌
+  // const resetBtn = buttons[1];
+
+  export {buttons};
