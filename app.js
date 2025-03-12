460840a8845ca9195afdbdf8b26f7dd4e856359f
JeonSunil <jeonsunil1225@gmail.com>
2025-03-12 12:39:53 +0900

    Feat: 서버실행시, data.json파일 생성 - 서버 실행시, data.json파일이 없을 경우 printRank모듈에서 일어나는 오류를 잡기 위해서, 서버를 실행하면 data.json파일이 없는 상황에서 서버를 실행하면, 빈배열의 data.json파일이 생성되게 만듦.

--- app.js
+++ app.js
@@ -10,38 +10,46 @@ const __dirname = path.resolve();
 const app = express();
 let jsonData = [];
 
-//* index파일을 불러올 때 필요한 파일을 참조한다.
-app.use('/public', express.static('public'));
-
-//* / 요청이 들어올 경우 index화면을 표시한다.
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-})
-
-app.get('/data.json', (req, res) => {
-  res.sendFile(__dirname + '/data.json');
-})
-
-app.post('/data', (req, res) => {
-  req.on('data', (data) => {
-    let ParData = qs.parse(data.toString());
-    console.log(ParData);
-    jsonData.push(ParData);
-   if(fs.existsSync('data.json')) {
-      jsonData = JSON.parse(fs.readFileSync('data.json'));
-      console.log(jsonData);
+function gameServer() {
+  //* index파일을 불러올 때 필요한 파일을 참조한다.
+  app.use('/public', express.static('public'));
+  
+  //* / 요청이 들어올 경우 index화면을 표시한다.
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+  })
+  
+  app.get('/data.json', (req, res) => {
+    res.sendFile(__dirname + '/data.json');
+  })
+  
+  app.post('/data', (req, res) => {
+    req.on('data', (data) => {
+      let ParData = qs.parse(data.toString());
+      console.log(ParData);
       jsonData.push(ParData);
-    fs.writeFileSync('data.json', JSON.stringify(jsonData, null, 2), 'utf-8');
-    } else {
+     if(fs.existsSync('data.json')) {
+        jsonData = JSON.parse(fs.readFileSync('data.json'));
+        console.log(jsonData);
+        jsonData.push(ParData);
       fs.writeFileSync('data.json', JSON.stringify(jsonData, null, 2), 'utf-8');
-    }
-
+      } else {
+        fs.writeFileSync('data.json', JSON.stringify(jsonData, null, 2), 'utf-8');
+      }
+  
+    })
+    req.on('end', () => {
+      res.redirect(302, 'http://localhost:3000/');
+    })
   })
-  req.on('end', () => {
-    res.redirect(302, 'http://localhost:3000/');
-  })
-})
+  };
 
+if(!fs.existsSync('data.json')) {
+  fs.writeFileSync('data.json', JSON.stringify(jsonData, null, 2), 'utf-8');
+  gameServer();
+} else {
+  gameServer();
+}
 //* 서버 구동 
 app.listen(3000, () => {
   console.log(`서버 실행 중.. (http://localhost:3000)`);
